ff7adbfcc0b5fb3ad730d755f3a105701d96ace1
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2024-10-14 22:19:50 -0300

    Refactor cpu

--- tests/pychip8/test_cpu.py
+++ tests/pychip8/test_cpu.py
@@ -1,18 +1,24 @@
+from collections.abc import Callable
 from io import BytesIO
 from random import randint
+from typing import cast
 from unittest.mock import MagicMock
 
+from pychip8.core import Chip8Core
 from pychip8.cpu import Chip8
+from pychip8.devices.devicebus import DeviceBus
+from pychip8.devices.display import Display
+from pychip8.devices.keyboard import Keyboard
 
 
 class TestChip8:
     def test_repr(self) -> None:
         for i in range(10):
             sut = Chip8(
-                cores=[MagicMock() for _ in range(i)],
-                bus=MagicMock(),
-                display=MagicMock(),
-                keyboard=MagicMock(),
+                cores=[MagicMock(spec_set=Chip8Core) for _ in range(i)],
+                bus=MagicMock(spec_set=DeviceBus),
+                display=MagicMock(spec_set=Display),
+                keyboard=MagicMock(spec_set=Keyboard),
                 instructions_per_update=16,
             )
 
@@ -21,10 +27,10 @@ class TestChip8:
     def test_length(self) -> None:
         for i in range(10):
             sut = Chip8(
-                cores=[MagicMock() for _ in range(i)],
-                bus=MagicMock(),
-                display=MagicMock(),
-                keyboard=MagicMock(),
+                cores=[MagicMock(spec_set=Chip8Core) for _ in range(i)],
+                bus=MagicMock(spec_set=DeviceBus),
+                display=MagicMock(spec_set=Display),
+                keyboard=MagicMock(spec_set=Keyboard),
                 instructions_per_update=16,
             )
 
@@ -35,19 +41,19 @@ class TestChip8:
             content = [randint(0, 255) for _ in range(randint(1, 512))]
             program = BytesIO(bytes(content))
 
-            bus = MagicMock()
+            mock_bus = MagicMock(spec_set=DeviceBus)
 
             sut = Chip8(
-                cores=[MagicMock()],
-                bus=bus,
-                display=MagicMock(),
-                keyboard=MagicMock(),
+                cores=[MagicMock(spec_set=Chip8Core)],
+                bus=mock_bus,
+                display=MagicMock(spec_set=Display),
+                keyboard=MagicMock(spec_set=Keyboard),
                 instructions_per_update=16,
             )
 
             sut.load_program(program)
 
-            bus.load_program.assert_called_once_with(program, 0x200)
+            mock_bus.load_program.assert_called_once_with(program, 0x200)
 
     def test_load_program_with_load_at(self) -> None:
         for _ in range(10):
@@ -55,30 +61,30 @@ class TestChip8:
             content = [randint(0, 255) for _ in range(randint(1, 512))]
             program = BytesIO(bytes(content))
 
-            bus = MagicMock()
+            mock_bus = MagicMock(spec_set=DeviceBus)
 
             sut = Chip8(
-                cores=[MagicMock()],
-                bus=bus,
-                display=MagicMock(),
-                keyboard=MagicMock(),
+                cores=[MagicMock(spec_set=Chip8Core)],
+                bus=mock_bus,
+                display=MagicMock(spec_set=Display),
+                keyboard=MagicMock(spec_set=Keyboard),
                 instructions_per_update=16,
             )
 
             sut.load_program(program, load_at=address)
 
-            bus.load_program.assert_called_once_with(program, address)
+            mock_bus.load_program.assert_called_once_with(program, address)
 
     def test_tick(self) -> None:
         for i in range(10):
             instructions_per_update = randint(1, 16)
-            cores = [MagicMock() for _ in range(i)]
+            cores = [MagicMock(spec_set=Chip8Core) for _ in range(i)]
 
             sut = Chip8(
-                cores=cores,  # type: ignore  # noqa: PGH003
-                bus=MagicMock(),
-                display=MagicMock(),
-                keyboard=MagicMock(),
+                cores=cast(list[Chip8Core], cores),
+                bus=MagicMock(spec_set=DeviceBus),
+                display=MagicMock(spec_set=Display),
+                keyboard=MagicMock(spec_set=Keyboard),
                 instructions_per_update=16,
             )
 
@@ -91,16 +97,16 @@ class TestChip8:
     def test_callbacks_in_tick(self) -> None:
         for i in range(10):
             instructions_per_update = randint(1, 16)
-            cores = [MagicMock() for _ in range(i)]
+            cores = [MagicMock(spec_set=Chip8Core) for _ in range(i)]
 
-            tick_callback = MagicMock()
-            update_callback = MagicMock()
+            tick_callback = MagicMock(spec_set=Callable)
+            update_callback = MagicMock(spec_set=Callable)
 
             sut = Chip8(
-                cores=cores,  # type: ignore  # noqa: PGH003
-                bus=MagicMock(),
-                display=MagicMock(),
-                keyboard=MagicMock(),
+                cores=cast(list[Chip8Core], cores),
+                bus=MagicMock(spec_set=DeviceBus),
+                display=MagicMock(spec_set=Display),
+                keyboard=MagicMock(spec_set=Keyboard),
                 instructions_per_update=instructions_per_update,
             )
             sut.set_tick_callback(tick_callback)
@@ -113,25 +119,25 @@ class TestChip8:
                 assert update_callback.call_count == j // instructions_per_update
 
     def test_display(self) -> None:
-        mock_display = MagicMock()
+        mock_display = MagicMock(spec_set=Display)
 
         sut = Chip8(
-            cores=[MagicMock()],
-            bus=MagicMock(),
+            cores=[MagicMock(spec_set=Chip8Core)],
+            bus=MagicMock(spec_set=DeviceBus),
             display=mock_display,
-            keyboard=MagicMock(),
+            keyboard=MagicMock(spec_set=Keyboard),
             instructions_per_update=16,
         )
 
         assert sut.display is mock_display
 
     def test_keyboard(self) -> None:
-        mock_keyboard = MagicMock()
+        mock_keyboard = MagicMock(spec_set=Keyboard)
 
         sut = Chip8(
-            cores=[MagicMock()],
-            bus=MagicMock(),
-            display=MagicMock(),
+            cores=[MagicMock(spec_set=Chip8Core)],
+            bus=MagicMock(spec_set=DeviceBus),
+            display=MagicMock(spec_set=Display),
             keyboard=mock_keyboard,
             instructions_per_update=16,
         )
